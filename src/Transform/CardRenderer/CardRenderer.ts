ae54e1d221fe0c90414593e0f99628473edced9f
Kanchan Verma <kaverma@github.com>
2020-04-16 14:36:21 +0530

    Added contracts for some types

--- src/Transform/CardRenderer/CardRenderer.ts
+++ src/Transform/CardRenderer/CardRenderer.ts
@@ -1,15 +1,19 @@
-var Handlebars = require("handlebars");
-var http = require('http');
-var fs = require('fs');
+const Handlebars = require("handlebars");
+const http = require('http');
+const fs = require('fs');
 const path = require("path");
+import {ClientType, SourceType, TemplateType} from '../TransformContracts';
 
 export class CardRenderer {
     
     constructor() {
     }
 
-    public ConstructCardJson(sourceType: string, clientType: string, eventJson: any): string {
+    public ConstructCardJson(sourceType: SourceType, clientType: ClientType, eventJson: any): string {
         
+    // TODO: Validations for inputs -
+    // sourceType, clientType and eventJson to be a valid json 
+
     var source = fs.readFileSync(path.resolve(__dirname, "../../CardTemplates/Teams/Handlebars/issue_opened.handlebars"), "utf8");
     var template = Handlebars.compile(source); 
 
--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,5 @@ import { testHandlebars } from './handlebars-test';
 
 export {testHandlebars} from './handlebars-test';
 export {CardRenderer} from './Transform/CardRenderer/CardRenderer';
-
+export {ClientType, SourceType, TemplateType} from './Transform/TransformContracts'
 testHandlebars();
