7b0b7a5f99fd11d07b8796cdad105377c31d3734
unknown <localadmin@IDC20Intern146.fareast.corp.microsoft.com>
2020-05-22 14:04:59 +0530

    changes to action/index.ts

--- src/Action/index.ts
+++ src/Action/index.ts
@@ -4,6 +4,7 @@ import * as github from '@actions/github';
 import CardRenderer from '../Transformer/CardRenderer/CardRenderer';
 import TemplateManager from '../TemplateManager';
 import { ClientType, TemplateType } from '../Transformer/Core/TransformContract';
+import { resolve } from 'dns';
 
 const TemplateTypeMap = new Map<string, TemplateType>(Object.entries(TemplateType).map(
   ([key, value]:[string, TemplateType]) => [key, value],
@@ -20,35 +21,37 @@ function throwIfUndefined<T>(value: T|undefined): T {
   throw new Error('Undefined value found');
 }
 
-async function run(): Promise<void> {
-  try {
+async function run(){
+  const renderedTemplate = new Promise<string>((resolve, reject) => {
     const repoName: string = core.getInput('repoName');
     const branch: string = core.getInput('branchName');
     const configName: string = core.getInput('templateConfigName');
-    //const data: string = core.getInput('data');
-    const data: string = JSON.stringify(github.context.payload, undefined, 2)
-    console.log(data);
-    const dataJson: JSON = JSON.parse(data);
-    const templateTypeString = core.getInput('templateType');
-    const templateType: TemplateType = throwIfUndefined<TemplateType>(
-      TemplateTypeMap.get(templateTypeString),
-    );
-    const sourceType: string = core.getInput('sourceType');
-    //console.log(sourceType);
-    const clientTypeString: string = core.getInput('clientType');
-    const clientType: ClientType = throwIfUndefined<ClientType>(
-      ClientTypeMap.get(clientTypeString),
-    );
-    await TemplateManager.setupTemplateConfigurationFromRepo(repoName, branch, configName);
-    const cardRenderer = new CardRenderer();
-    const renderedTemplate = await cardRenderer.ConstructCardJson(templateType,
-      sourceType, clientType, dataJson);
-    console.log(renderedTemplate);
-    core.setOutput('renderedTemplate', renderedTemplate);
-  } catch (error) {
-    core.setFailed(error);
-  }
+    const data: string = JSON.stringify(github.context.payload, undefined, 2);
+    resolve(data);
+  });
+  return renderedTemplate;
 }
+//renderedTemplate = new Promise<string> ( () => {
+//    const repoName: string = core.getInput('repoName');
+//    const branch: string = core.getInput('branchName');
+//    const configName: string = core.getInput('templateConfigName');
+//    const data: string = JSON.stringify(github.context.payload, undefined, 2)
+//    const dataJson: JSON = JSON.parse(data);
+//    const templateTypeString = core.getInput('templateType');
+//    const templateType: TemplateType = throwIfUndefined<TemplateType>(
+//      TemplateTypeMap.get(templateTypeString),
+//    );
+//    const sourceType: string = core.getInput('sourceType');
+//    const clientTypeString: string = core.getInput('clientType');
+//    const clientType: ClientType = throwIfUndefined<ClientType>(
+//    ClientTypeMap.get(clientTypeString),
+//    );
+//    TemplateManager.setupTemplateConfigurationFromRepo(repoName, branch, configName);
+//    const cardRenderer = new CardRenderer();
+//    const renderedTemplate = cardRenderer.ConstructCardJson(templateType,
+//      sourceType, clientType, dataJson);
+//);
+//}
 
-
-run();
+const renderedTemplate = run();
+core.setOutput('renderedTemplate', renderedTemplate)
