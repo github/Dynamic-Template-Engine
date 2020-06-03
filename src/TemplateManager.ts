c12c7ac3eb7e3b88affec3fa4538139aaa604207
Anuraag Puri <anuraag016@github.com>
2020-06-03 15:00:09 +0530

    Adding license text in code

--- src/TemplateManager.ts
+++ src/TemplateManager.ts
@@ -1,3 +1,4 @@
+/** Copyright (c) 2020 GitHub. This code is licensed under MIT license (see LICENSE(https://github.com/github/event-transformer/blob/feature/chatops/LICENSE) for details) */
 /* eslint-disable @typescript-eslint/no-throw-literal */
 import Transformer from './Transformer/Core/Transformer';
 import TransformerConfig from './Transformer/Model/TransformerConfig';
