f463ccd2840fd293ab84bd157af888a66e3f5084
meghnasavit <meghna.savit99@gmail.com>
2020-06-03 15:38:01 +0530

    check constructing event json

--- src/Action/index.ts
+++ src/Action/index.ts
@@ -37,8 +37,10 @@ async function run(): Promise<void> {
       ClientTypeMap.get(clientTypeString),
     );
     await TemplateManager.setupTemplateConfigurationFromRepo(repoName, branch, null, null, null);
-    const cardRenderer = new CardRenderer();
-    const renderedTemplate = await cardRenderer.ConstructCardJson(templateType, sourceType, clientType, dataJson);
+    //const cardRenderer = new CardRenderer();
+    //const renderedTemplate = await cardRenderer.ConstructCardJson(templateType, sourceType, clientType, dataJson);
+    const eventTransformer = new EventTransformer();
+    const renderedTemplate = eventTransformer.ConstructEventJson(templateType, sourceType, dataJson);
     console.log(renderedTemplate);
     core.setOutput('renderedTemplate', renderedTemplate);
   } catch (error) {
