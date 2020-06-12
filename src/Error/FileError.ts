b84b2175cae1bc59cff5e450325e2aa36f538c41
Anuraag Puri <anuraag016@github.com>
2020-06-12 13:10:22 +0530

    Adding documentation and build workflow (#7)
    * Create CONTRIBUTING.md
    
    * Create CODE_OF_CONDUCT.md
    
    * Create SECURITY.md
    
    * Adding base Readme
    
    * Adding Code of conduct, contributing and License
    
    * Fixing template config example type
    
    * Update package.json
    
    * Create CODEOWNERS.
    
    * Adding license text in code
    
    * Update README.md
    
    * Add build workflow
    
    * Added feature/chatops branch in workflow file
    
    * Moving some dependencies to devDependencies
    
    * Update CODEOWNERS
    
    * Copyright line comment in all files
    
    * PR comments: Adding windows and mac build to workflow

--- src/Error/FileError.ts
+++ src/Error/FileError.ts
@@ -1,3 +1,4 @@
+// Copyright (c) 2020 GitHub. This code is licensed under MIT license (see LICENSE(https://github.com/github/event-transformer/blob/feature/chatops/LICENSE) for details)
 export class EmptyFileError extends Error {
   constructor(message: string) {
     super(message);
--- src/Transformer/Model/EventTransformConfigEntry.ts
+++ src/Transformer/Model/EventTransformConfigEntry.ts
@@ -1,3 +1,4 @@
+// Copyright (c) 2020 GitHub. This code is licensed under MIT license (see LICENSE(https://github.com/github/event-transformer/blob/feature/chatops/LICENSE) for details)
 import BaseTransformConfigEntry from './BaseTransformConfigEntry';
 
 export default interface EventTransformConfigEntry extends BaseTransformConfigEntry{
