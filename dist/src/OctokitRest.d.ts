9d3aa87995a5b09b2b05b56426eeb898f659d213
meghnasavit <meghna.savit99@gmail.com>
2020-06-18 12:00:33 +0530

    removed unwanted declaration files

--- dist/src/OctokitRest.d.ts
+++
@@ -1,4 +0,0 @@
-export default class octokit {
-    static getTransformerConfig(repo: string, branch: string): Promise<any>;
-    static getTemplateFile(repo: string, branch: string, filePath: string): Promise<string>;
-}
