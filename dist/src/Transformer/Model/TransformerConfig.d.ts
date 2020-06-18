a4658e93cb3b1a88e8ab82e8a9249862995f8a5a
meghnasavit <meghna.savit99@gmail.com>
2020-06-18 12:06:23 +0530

    declaration file changes

--- dist/src/Transformer/Model/TransformerConfig.d.ts
+++
@@ -1,9 +0,0 @@
-/** Copyright (c) 2020 GitHub. This code is licensed under MIT license (see LICENSE(https://github.com/github/event-transformer/blob/feature/chatops/LICENSE) for details) */
-import CardRendererConfigEntry from './CardRendererConfigEntry';
-import EventTransformConfigEntry from './EventTransformConfigEntry';
-import PartialTemplateConfigEntry from './PartialTemplateConfigEntry';
-export default interface TransformerConfig {
-    cardRenderer: CardRendererConfigEntry[];
-    partials: PartialTemplateConfigEntry[];
-    eventTransformer: EventTransformConfigEntry[];
-}
