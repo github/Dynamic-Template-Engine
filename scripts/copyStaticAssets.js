4160d4e4fc37ed6d9c7eb05f485ab31b80127fdc
Anuraag Puri <anuraag016@github.com>
2020-07-08 10:45:34 +0530

    Removing test folder from tsconfig

--- scripts/copyStaticAssets.js
+++ scripts/copyStaticAssets.js
@@ -1,6 +1,8 @@
 const shell = require('shelljs');
 
-shell.cp("-R", "src/CardTemplate", "lib/src/CardTemplate");
-shell.cp("-R", "src/EventTemplate", "lib/src/EventTemplate");
-shell.cp("-R", "src/TransformerConfig.json", "lib/src/TransformerConfig.json");
+shell.mkdir("-p","lib/CardTemplate");
+shell.mkdir("-p","lib/EventTemplate");
+shell.cp("-R", "src/CardTemplate", "lib/CardTemplate");
+shell.cp("-R", "src/EventTemplate", "lib/EventTemplate");
+shell.cp("-R", "src/TransformerConfig.json", "lib/TransformerConfig.json");
 
